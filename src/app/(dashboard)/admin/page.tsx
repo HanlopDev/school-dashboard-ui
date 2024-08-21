620d5b4702d0c050a7599c6adc692ac71a4e6d1f
HanlopDev <hanafbomar@gmail.com>
2024-08-21 13:19:56 +0300

    initialize CountChart for students

---
+++ src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,34 @@
+import CountChart from "@/components/CountChart"
+import UserCard from "@/components/UserCard"
+
+const AdminPage = () => {
+  return (
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* LEFT */}
+      <div className="w-full lg:h-2/3">
+        {/* user card */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student"/>
+          <UserCard type="teacher"/>
+          <UserCard type="parent"/>
+          <UserCard type="staff"/>
+        </div>
+        {/* Middle chart */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* Count Chart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart/>
+          </div>
+          {/* Attendance Chart */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/* Bottom chart */}
+        <div className=""></div>
+      </div>
+      {/* RIGHT */}
+    <div className="w-full lg:h-1/3">R</div>
+    </div>
+  )
+}
+
+export default AdminPage
