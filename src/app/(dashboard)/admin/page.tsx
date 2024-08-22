33d26e4644cb3c39a7054432561c6d0c65e6ca22
HanlopDev <hanafbomar@gmail.com>
2024-08-22 10:08:02 +0300

    finalizing admin dashboard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,40 +1,45 @@
-import AttendanceChart from "@/components/AttendanceChart"
-import CountChart from "@/components/CountChart"
-import FinanceChart from "@/components/FinanceChart"
-import UserCard from "@/components/UserCard"
+import Announcement from "@/components/Announcement";
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountChart";
+import EventCalendar from "@/components/EventCalendar";
+import FinanceChart from "@/components/FinanceChart";
+import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
   return (
-    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
-      <div className="w-full lg:h-2/3 flex flex-col gap-8">
-        {/* user card */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
         <div className="flex gap-4 justify-between flex-wrap">
-          <UserCard type="student"/>
-          <UserCard type="teacher"/>
-          <UserCard type="parent"/>
-          <UserCard type="staff"/>
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
         </div>
-        {/* Middle chart */}
+        {/* MIDDLE CHARTS */}
         <div className="flex gap-4 flex-col lg:flex-row">
-          {/* Count Chart */}
+          {/* COUNT CHART */}
           <div className="w-full lg:w-1/3 h-[450px]">
-            <CountChart/>
+            <CountChart />
           </div>
-          {/* Attendance Chart */}
+          {/* ATTENDANCE CHART */}
           <div className="w-full lg:w-2/3 h-[450px]">
-            <AttendanceChart/>
+            <AttendanceChart />
           </div>
         </div>
-        {/* Bottom chart */}
+        {/* BOTTOM CHART */}
         <div className="w-full h-[500px]">
-          <FinanceChart/> 
+          <FinanceChart />
         </div>
       </div>
       {/* RIGHT */}
-    <div className="w-full lg:h-1/3">R</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcement/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
---
+++ src/components/Announcement.tsx
@@ -0,0 +1,47 @@
+const Announcement = () => {
+  return (
+    <div className='bg-white rounded-md p-4'>
+        <div className="flex items-center justify-between">
+            <h1 className="text-xl font-semibold">Announcements</h1>
+            <span className="text-xs text-gray-400">view all</span>
+        </div>
+        <div className="flex flex-col gap-4 mt-4">
+            <div className="bg-lamaSkyLight rounded-md p-4">
+                <div className="flex items-center justify-between">
+                    <h2 className="font-medium">Lorem ipsum dolor sit.</h2>
+                    <span className="text-sm text-gray-400 bg-white rounded-md px-1 py-1">
+                        2024-1-2
+                    </span>
+                </div>
+                <p className="text-sm text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Reprehenderit qui quidem amet autem non est? Natus, 
+                    eligendi a! Earum, aperiam.</p>
+            </div>
+            <div className="bg-lamaPurpleWhite rounded-md p-4">
+                <div className="flex items-center justify-between">
+                    <h2 className="font-medium">Lorem ipsum dolor sit.</h2>
+                    <span className="text-sm text-gray-400 bg-white rounded-md px-1 py-1">
+                        2024-1-2
+                    </span>
+                </div>
+                <p className="text-sm text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Reprehenderit qui quidem amet autem non est? Natus,
+                    eligendi a! Earum, aperiam.</p>
+            </div>
+            <div className="bg-lamaYellowLight rounded-md p-4">
+                <div className="flex items-center justify-between">
+                    <h2 className="font-medium">Lorem ipsum dolor sit.</h2>
+                    <span className="text-sm text-gray-400 bg-white rounded-md px-1  py-1">
+                        2024-1-2
+                    </span>
+                </div>
+                <p className="text-sm text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Reprehenderit qui quidem amet autem non est? Natus,
+                    eligendi a! Earum, aperiam.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Announcement
